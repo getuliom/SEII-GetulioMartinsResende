f7ba520dc4888bcd46e575c2a2f061861818b7e4
Getúlio Martins <getulio.martinsresende@gmail.com>
2021-03-15 20:20:56 -0300

    operadores aritméticos

--- Semana02/prog02.py
+++ Semana02/prog02.py
@@ -1,3 +1,7 @@
-num=3.14
-
-print(type(num))
+print(3+2) #soma
+print(3-2) #subtração
+print(3*2) #multiplicação
+print(3/2) #divisão
+print(3//2) #divisão para baixo
+print(3**2) #exponencial
+print(3%2) #modulo
