e061304327cc2ad489b1920f5ae70dabb8a9960b
Getúlio Martins <getulio.martinsresende@gmail.com>
2021-03-15 20:27:51 -0300

    soma

--- Semana02/prog02.py
+++ Semana02/prog02.py
@@ -1,7 +1,3 @@
-print(3+2) #soma
-print(3-2) #subtração
-print(3*2) #multiplicação
-print(3/2) #divisão
-print(3//2) #divisão para baixo
-print(3**2) #exponencial
-print(3%2) #modulo
+num=1
+num+=1
+print(num)
