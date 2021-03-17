3ca64657cdfaf7e098305c20b91a28372eed4c0f
Getúlio Martins <getulio.martinsresende@gmail.com>
2021-03-16 22:24:50 -0300

    adicionando os marcadores dos eixos
    Também foi corrigido os parênteses do plt title

--- Semana02/matplot01.py
+++ Semana02/matplot01.py
@@ -2,12 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np 
 import pandas as pd
 
-x=[1,2,3]
-y=[2,4,6]
+x=[0,1,2,3,4]
+y=[0,2,4,6,8]
 plt.plot(x,y)
 
-plt.title('Our first Graph!'),fontdict={'fontname':'Comic Sans MS','fontsize':20})
+plt.title('Our first Graph!',fontdict={'fontname':'Comic Sans MS','fontsize':20})
 plt.xlabel('X Axis')
 plt.ylabel('Y Axis')
 
+plt.xticks([0,1,2,3])
+plt.yticks([0,2,4,6,8,10])
 plt.show()
