78cc8574e9395a6bb7328c1fb44320f93f166bd1
Getúlio Martins <getulio.martinsresende@gmail.com>
2021-03-16 22:34:50 -0300

    passando parametros para o plt

--- Semana02/matplot01.py
+++ Semana02/matplot01.py
@@ -4,7 +4,10 @@ import pandas as pd
 
 x=[0,1,2,3,4]
 y=[0,2,4,6,8]
-plt.plot(x,y)
+#plt.plot(x,y,label='2x',color='red',linewidth=2,marker='.',linestyle='--',markersize=10,markeredgecolor='blue')
+# Use shorthand notation
+#fmt='[color][marker][line]'
+plt.plot(x,y,'b*--',label='2x')
 
 plt.title('Our first Graph!',fontdict={'fontname':'Comic Sans MS','fontsize':20})
 plt.xlabel('X Axis')
@@ -12,4 +15,7 @@ plt.ylabel('Y Axis')
 
 plt.xticks([0,1,2,3])
 plt.yticks([0,2,4,6,8,10])
+
+plt.legend()
+
 plt.show()
