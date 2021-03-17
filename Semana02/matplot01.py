1a4ad30b7d26ba95d77ea49e6e12851a94ba343e
Getúlio Martins <getulio.martinsresende@gmail.com>
2021-03-16 22:40:40 -0300

    adicionando uma segunda função no gráfico

--- Semana02/matplot01.py
+++ Semana02/matplot01.py
@@ -9,12 +9,18 @@ y=[0,2,4,6,8]
 #fmt='[color][marker][line]'
 plt.plot(x,y,'b*--',label='2x')
 
+## Line Number Two
+x2=np.arange(0,4.5,0.5)
+print(x2)
+plt.plot(x2[:5],x2[:5]**2,'r',label='x²')
+plt.plot(x2[4:],x2[4:]**2,'r--')
+
 plt.title('Our first Graph!',fontdict={'fontname':'Comic Sans MS','fontsize':20})
 plt.xlabel('X Axis')
 plt.ylabel('Y Axis')
 
 plt.xticks([0,1,2,3])
-plt.yticks([0,2,4,6,8,10])
+#plt.yticks([0,2,4,6,8,10])
 
 plt.legend()
 
