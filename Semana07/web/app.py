c0631fdf058a8cea04e7332ec2762eb7b9b00e41
Getúlio Martins <getulio.martinsresende@gmail.com>
2021-04-16 20:49:09 -0300

    Made some modifications (24:54 - Youtube)

--- Semana07/web/app.py
+++ Semana07/web/app.py
@@ -1,8 +1,21 @@
 from flask import Flask,render_template,url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
+db=SQLAlchemy(app)
 
-@app.route('/')
+class Todo(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    content=db.Column(db.String(200),nullable=False)
+    date_created=db.Column(db.DateTime,default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+
+@app.route('/',methods=['POST','GET'])
 def index():
     return render_template('index.html')
 
